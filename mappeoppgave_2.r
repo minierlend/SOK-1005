526f997d2ff70a980115e87440fe35516eab4cf5
minierlend <eol112@uit.no>
2022-02-07 14:14:00 +0100

    Create mappeoppgave_2.r

---
+++ mappeoppgave_2.r
@@ -0,0 +1,86 @@
+#Mappeoppgave 2
+
+
+install.packages("jsonlite")
+install.packages("scales") 
+install.packages("cluster")  
+
+
+library("jsonlite")
+library(ggplot2)                                 # Install and load scales, prøvde å laste ned scale prosent pakke
+library("scales")
+library(tidyverse)
+library(cluster)                              #prøver denne for å lage mindre skrift
+
+
+Covid_death <- 
+  fromJSON(readLines('https://static01.nyt.com/newsgraphics/2021/12/20/us-coronavirus-deaths-2021/ff0adde21623e111d8ce103fedecf7ffc7906264/scatter.json')[1])
+
+##oppgave 1
+
+##gjør om til prosent ved bruk av mutate formelen, trenger ikke gjøre dette siden vi bruker scale_x_continuos
+Covid_death <- Covid_death %>% 
+  mutate(fully_vaccinated_pct_of_pop_percentage=fully_vaccinated_pct_of_pop*100)
+
+
+##ggplot for å prøve å gjennskape bildet vi fikk utdelt.
+
+Covid_death %>% 
+  ggplot(aes(x= fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name, color=name)) +
+  geom_text(hjust=0, vjust=0)+
+  geom_point() +
+  theme(text = element_text(size=10)) +
+  labs(title="Korona dødsfall per 100k innbygggere iforhold til vaksjonasjonsgrad")+
+  xlab("vaksinerte i populasjonen i %") +
+  ylab("Dødsfall per 100k") +
+  scale_x_continuous(labels = scales::percent, limits = c(0.45, 0.8), 
+                     breaks = c(0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80))+
+  theme_bw() +
+  theme(legend.position = "none") 
+
+        
+
+
+
+##plot2 <- agnes(Covid_death, metric =  "manhattan", stand = TRUE)
+
+##plot(plot2, which.plots = 2)
+
+
+###-----
+
+#oppgave 2)
+
+Covid_death %>% 
+  ggplot(aes(x= fully_vaccinated_pct_of_pop, y=deaths_per_100k, label = name, color = name)) +
+  geom_point() +
+  geom_text(hjust=0, vjust=0)+
+  scale_x_continuous(labels = scales::percent, limits = c(0.45, 0.8), 
+                     breaks = c(0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80)) +
+  geom_smooth(method = lm, col = "blue", alpha = 0.2) + 
+  theme_bw() +
+  theme(legend.position = "none") +
+  labs(title="Korona dødsfall per 100k innbygggere iforhold til vaksjonasjonsgrad")+
+  xlab("vaksinerte i populasjonen i prosent") +
+  ylab("Dødsfall per 100k") 
+
+
+
+lm(deaths_per_100k ~ fully_vaccinated_pct_of_pop_percentage, data=Covid_death) 
+
+
+## som vi ser på linja er at det er en tydelig korrelasjon mellom vaksineringsgrad og færre dødsfall.
+#Jo mindre vaksinerte i populasjonen, jo større sannsynlighet er det og dø av COVID-19 viruset. 
+# vi tolker tallene slik:
+#det er en negativ linære korrelasjon i dette tilfellet, men har høy spredning. 
+#hvis man skal tro på denne modellen, som vi gjør. vil det si at hvis 45% prosent av populasjonen er vaksinerte,
+#vil nesten 15 personer dø per 100k. Intercept er bare tallet der dødsfall per 100k treffer y aksen. så det vil si:
+#hvis 0 prosent av populasjonen er vaksinert, vil 31 personer død hvis vi følger vår regressjonsmodell. 
+
+#det negative tallet indikerer at når uavhengige tallet blir større, vil det avhengige tallet bli mindre. 
+#vi ser at tallet er -0.3666, jo nærmere dette tallet er 1 eller -1 jo sterkere sammenheng vil det ha, det 
+#oppsummerer graden av samsvar. I dette eksempelet har vi målt graden av samsvar hvor mange som  dør iforhold til 
+#vaksinasjonsgrad. siden det er et negativt tall betyr det at det er negativ korrelasjon, og regresjonslinja går nedover. 
+
+  
+##----
